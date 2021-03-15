4f2fe90c408ea316ef36a2ea80d754a123ab0007
Garnet DeGelder <garnet@garnetdg.ca>
2021-03-15 10:52:23 -0400

    Add database schema

---
+++ database.sql
@@ -0,0 +1,25 @@
+PRAGMA foreign_keys=on;
+
+BEGIN TRANSACTION;
+
+CREATE TABLE IF NOT EXISTS "users" (
+	"id" INTEGER PRIMARY KEY NOT NULL,
+	"username" TEXT NOT NULL UNIQUE,
+	"password" TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS "tracked_locations" (
+	"id" INTEGER PRIMARY KEY NOT NULL,
+	"name" TEXT NOT NULL,
+	"ping_key" TEXT NOT NULL UNIQUE,
+	"trusted" INTEGER NOT NULL DEFAULT 1
+);
+
+CREATE TABLE IF NOT EXISTS "address_history" (
+	"entry_id" INTEGER PRIMARY KEY NTO NULL,
+	"location_id" INTEGER NOT NULL REFERENCES "tracked_locations"("id") ON UPDATE CASCADE ON DELETE CASCADE,
+	"ip_address" TEXT,
+	UNIQUE("location_id", "ip_address") ON CONFLICT REPLACE
+);
+
+COMMIT TRANSACTION;
